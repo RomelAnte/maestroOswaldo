c9cdacd762769bc5989f6d1bdb4d795da32d48f8
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2024-11-03 12:03:47 -0500

    responcive movil

--- js/Main.js
+++ js/Main.js
@@ -13,3 +13,21 @@ nextButton.addEventListener('click', () => {
   slideIndex = Math.min(slideIndex + 1, slider.children.length - 1);
   slider.style.transform = `translateX(-${slideIndex * 100}%)`;
 });
+
+var currentSlide = 0;
+const slides = document.querySelectorAll('.cursos__img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  const carouselContainer = document.querySelector('.cursos__');
+  carouselContainer.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
